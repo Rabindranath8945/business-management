8222db836be5b6cc78ff771375817165afb658ad
rabindranath8945 <connectrnmwd@gmail.com>
2026-06-11 21:02:06 +0530

    Sales module updated

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ require("dotenv").config();
 const app = express();
 const productRoutes = require("./routes/productRoutes");
 const purchaseRoutes = require("./routes/purchaseRoutes");
-const salesRoutes = require("./routes/salesRoute");
+const saleRoutes = require("./routes/saleRoute");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/purchases", purchaseRoutes);
-app.use("api/sales", salesRoutes);
+app.use("api/sales", saleRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI)
